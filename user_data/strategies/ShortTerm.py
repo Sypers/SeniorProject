421594595707fe3e83b1f0b0397c4a08b4f439f5
Ahmad <aalahmary0032@stu.kau.edu.sa>
2022-10-02 22:22:15 +0300

    Experimenting with RSI and EMA  and custom stoploss and exit

--- user_data/strategies/ShortTerm.py
+++ user_data/strategies/ShortTerm.py
@@ -1,9 +1,15 @@
 # --- Do not remove these libs ---
+from datetime import datetime, timedelta
+from typing import Optional, Union
+
 import ta.trend
 import ta.utils
 import ta.momentum
+import ta.volatility
 from pandas import DataFrame
-from freqtrade.strategy import IStrategy
+
+from freqtrade.persistence import Trade
+from freqtrade.strategy import IStrategy, DecimalParameter, IntParameter
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 
 
@@ -12,15 +18,21 @@ import freqtrade.vendor.qtpylib.indicators as qtpylib
 
 class ShortTerm(IStrategy):
     INTERFACE_VERSION: int = 3
+
+    # Strategy Default values
+    BUY_RSI_DEF = 20
+    SELL_RSI_DEF = 80
     # Minimal ROI designed for the strategy.
     # adjust based on market conditions. We would recommend to keep it low for quick turn arounds
     # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
-        "20": 0.001,
-        "10": 0.002,
-        "5": 0.004,
-        "0": 0.005
+        "60": 0.002,
+        "30": 0.004,
+        "15": 0.006,
+        "0": 0.009
     }
+
+    use_custom_stoploss = True
 
     # Trailing stoploss
     trailing_stop = False
@@ -35,11 +47,13 @@ class ShortTerm(IStrategy):
     stoploss = -0.003
 
     # Optimal timeframe for the strategy
-    timeframe = '1m'
+    timeframe = '5m'
 
     # Strategy Parameters
-    buy_stoch = 23.0
-    sell_stoch = 77.0
+    # buy_stoch = DecimalParameter(1, 33, default=BUY_STOCH_DEF)
+    # sell_stoch = DecimalParameter(67, 100, default=SELL_STOCH_DEF)
+    buy_rsi = IntParameter(1, 34, default=BUY_RSI_DEF)
+    sell_rsi = IntParameter(66, 100, default=SELL_RSI_DEF)
 
     # Run "populate_indicators()" only for new candle.
     process_only_new_candles = True
@@ -50,25 +64,29 @@ class ShortTerm(IStrategy):
     ignore_roi_if_entry_signal = False
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        MACD = ta.trend.MACD(close=dataframe['close'], window_slow=8, window_fast=5)
-        PSAR = ta.trend.PSARIndicator(close=dataframe['close'], high=dataframe['high'], low=dataframe['low'])
-        MA = ta.trend.SMAIndicator(dataframe['close'], 100)
-        Stoch = ta.momentum.StochasticOscillator(high=dataframe['high'], low=dataframe['low'], close=dataframe['close'])
-        # dataframe['macd'] = MACD.macd()
-        dataframe['sma'] = MA.sma_indicator()
-        dataframe['macdhist'] = MACD.macd_diff()
-        dataframe['psar_up'] = PSAR.psar_up_indicator()
-        dataframe['psar_down'] = PSAR.psar_down_indicator()
-        dataframe['stoch'] = Stoch.stoch()
+        # MACD = ta.trend.MACD(close=dataframe['close'], window_slow=8, window_fast=5)
+        # Stoch = ta.momentum.StochasticOscillator(high=dataframe['high'], low=dataframe['low'], close=dataframe['close'])
+        ATR = ta.volatility.AverageTrueRange(dataframe['high'], dataframe['low'], dataframe['close'])
+        # MA = ta.trend.SMAIndicator(dataframe['close'], window=50)
+        RSI = ta.momentum.RSIIndicator(dataframe['close'], window=2)
+        EMA200 = ta.trend.EMAIndicator(dataframe['close'], window=200)
+        EMA50 = ta.trend.EMAIndicator(dataframe['close'], window=50)
+        # Appending indicator results to new dataframe columns
+        dataframe['atr'] = ATR.average_true_range()
+        # dataframe['macdhist'] = MACD.macd_diff()
+        # dataframe['stoch'] = Stoch.stoch()
+        # dataframe['ma'] = MA.sma_indicator()
+        dataframe['rsi'] = RSI.rsi()
+        dataframe['ema200'] = EMA200.ema_indicator()
+        dataframe['ema50'] = EMA50.ema_indicator()
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                    (dataframe['psar_up'] == 1) &
-                    (dataframe['macdhist'] > 0) &
-                    #  (qtpylib.crossed_above(dataframe['close'], dataframe['sma'])) &
-                    (dataframe['stoch'] < self.buy_stoch)
+                    (dataframe['ema50'] > dataframe['ema200']) &
+                    (dataframe['close'] > dataframe['ema200']) &
+                    (qtpylib.crossed_below(dataframe['rsi'], self.BUY_RSI_DEF))
             ),
             'enter_long'] = 1
         return dataframe
@@ -76,9 +94,31 @@ class ShortTerm(IStrategy):
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                    (dataframe['psar_down'] == 1) &
-                    (dataframe['macdhist'] < 0) &
-                    #  (qtpylib.crossed_below(dataframe['close'], dataframe['sma'])) &
-                    (dataframe['stoch'] > self.sell_stoch)),
+                    (dataframe['ema50'] < dataframe['ema200']) &
+                    (dataframe['close'] < dataframe['ema200']) &
+                    (qtpylib.crossed_above(dataframe['rsi'], self.SELL_RSI_DEF))
+            ),
             'exit_long'] = 1
         return dataframe
+
+    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
+                        current_profit: float, **kwargs) -> float:
+        if current_time - timedelta(minutes=60) > trade.open_date_utc:
+            return -0.0025
+        elif current_time - timedelta(minutes=30) > trade.open_date_utc:
+            return -0.005
+        elif current_time - timedelta(minutes=15) > trade.open_date_utc:
+            return -0.007
+        return 1
+
+    def custom_exit(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
+                    current_profit: float, **kwargs) -> Optional[Union[str, bool]]:
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        last_candle = dataframe.iloc[-1].squeeze()
+
+        if current_profit > 0.005:
+            if (last_candle['rsi'] < self.SELL_RSI_DEF) | (last_candle['atr'] >= 55):
+                return 'volatility_escape'
+        if 0.0001 < current_profit < 0.0005:
+            if last_candle['ema50'] <= last_candle['ema200']:
+                return 'ema_cross_sell'
