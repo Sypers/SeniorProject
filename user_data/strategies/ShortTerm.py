a01638a7d741eb0793ad9ec73d3b79329e6910d7
Ahmad <aalahmary0032@stu.kau.edu.sa>
2022-10-04 14:10:55 +0300

    Experimenting with EMA, Bollinger Bands, MACD

--- user_data/strategies/ShortTerm.py
+++ user_data/strategies/ShortTerm.py
@@ -19,17 +19,14 @@ import freqtrade.vendor.qtpylib.indicators as qtpylib
 class ShortTerm(IStrategy):
     INTERFACE_VERSION: int = 3
 
-    # Strategy Default values
-    BUY_RSI_DEF = 20
-    SELL_RSI_DEF = 80
     # Minimal ROI designed for the strategy.
     # adjust based on market conditions. We would recommend to keep it low for quick turn arounds
     # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
-        "60": 0.002,
-        "30": 0.004,
-        "15": 0.006,
-        "0": 0.009
+        "120": 0.03,
+        "60": 0.05,
+        "35": 0.067,
+        "0": 0.09
     }
 
     use_custom_stoploss = True
@@ -44,7 +41,7 @@ class ShortTerm(IStrategy):
     can_short = False
 
     # Optimal stoploss designed for the strategy
-    stoploss = -0.003
+    stoploss = -0.03
 
     # Optimal timeframe for the strategy
     timeframe = '5m'
@@ -52,8 +49,6 @@ class ShortTerm(IStrategy):
     # Strategy Parameters
     # buy_stoch = DecimalParameter(1, 33, default=BUY_STOCH_DEF)
     # sell_stoch = DecimalParameter(67, 100, default=SELL_STOCH_DEF)
-    buy_rsi = IntParameter(1, 34, default=BUY_RSI_DEF)
-    sell_rsi = IntParameter(66, 100, default=SELL_RSI_DEF)
 
     # Run "populate_indicators()" only for new candle.
     process_only_new_candles = True
@@ -64,61 +59,39 @@ class ShortTerm(IStrategy):
     ignore_roi_if_entry_signal = False
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # MACD = ta.trend.MACD(close=dataframe['close'], window_slow=8, window_fast=5)
-        # Stoch = ta.momentum.StochasticOscillator(high=dataframe['high'], low=dataframe['low'], close=dataframe['close'])
-        ATR = ta.volatility.AverageTrueRange(dataframe['high'], dataframe['low'], dataframe['close'])
-        # MA = ta.trend.SMAIndicator(dataframe['close'], window=50)
-        RSI = ta.momentum.RSIIndicator(dataframe['close'], window=2)
-        EMA200 = ta.trend.EMAIndicator(dataframe['close'], window=200)
-        EMA50 = ta.trend.EMAIndicator(dataframe['close'], window=50)
-        # Appending indicator results to new dataframe columns
-        dataframe['atr'] = ATR.average_true_range()
-        # dataframe['macdhist'] = MACD.macd_diff()
-        # dataframe['stoch'] = Stoch.stoch()
-        # dataframe['ma'] = MA.sma_indicator()
-        dataframe['rsi'] = RSI.rsi()
-        dataframe['ema200'] = EMA200.ema_indicator()
-        dataframe['ema50'] = EMA50.ema_indicator()
+        EMA = ta.trend.EMAIndicator(dataframe['close'], window=9)
+        BB = ta.volatility.BollingerBands(dataframe['close'], window=16)
+        MACD = ta.trend.MACD(dataframe['close'], window_slow=22, window_fast=8)
+        dataframe['ema'] = EMA.ema_indicator()
+        dataframe['bb_h'] = BB.bollinger_hband()
+        dataframe['bb_m'] = BB.bollinger_mavg()
+        dataframe['bb_l'] = BB.bollinger_lband()
+        dataframe['bb_w'] = BB.bollinger_wband()
+        dataframe['macd'] = MACD.macd()
+        dataframe['macdsignal'] = MACD.macd_signal()
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                    (dataframe['ema50'] > dataframe['ema200']) &
-                    (dataframe['close'] > dataframe['ema200']) &
-                    (qtpylib.crossed_below(dataframe['rsi'], self.BUY_RSI_DEF))
+                (qtpylib.crossed_above(dataframe['ema'], dataframe['bb_m']) |
+                 dataframe['close'] <= dataframe['bb_l']) &
+                ((dataframe['macd'] > 8) | (dataframe['macd'] < -8)) &
+                (dataframe['bb_w'] > 0.15)
             ),
             'enter_long'] = 1
+        # dataframe.loc[
+        #     (
+        #
+        #     ),
+        # ] = 1
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                    (dataframe['ema50'] < dataframe['ema200']) &
-                    (dataframe['close'] < dataframe['ema200']) &
-                    (qtpylib.crossed_above(dataframe['rsi'], self.SELL_RSI_DEF))
+                (qtpylib.crossed_below(dataframe['ema'], dataframe['bb_m']) |
+                dataframe['close'] >= dataframe['bb_h'])
             ),
             'exit_long'] = 1
         return dataframe
-
-    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
-                        current_profit: float, **kwargs) -> float:
-        if current_time - timedelta(minutes=60) > trade.open_date_utc:
-            return -0.0025
-        elif current_time - timedelta(minutes=30) > trade.open_date_utc:
-            return -0.005
-        elif current_time - timedelta(minutes=15) > trade.open_date_utc:
-            return -0.007
-        return 1
-
-    def custom_exit(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
-                    current_profit: float, **kwargs) -> Optional[Union[str, bool]]:
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        last_candle = dataframe.iloc[-1].squeeze()
-
-        if current_profit > 0.005:
-            if (last_candle['rsi'] < self.SELL_RSI_DEF) | (last_candle['atr'] >= 55):
-                return 'volatility_escape'
-        if 0.0001 < current_profit < 0.0005:
-            if last_candle['ema50'] <= last_candle['ema200']:
-                return 'ema_cross_sell'
