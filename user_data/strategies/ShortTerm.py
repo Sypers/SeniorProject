f9e42bf031090e8f1b22c129bcf180c182712ceb
Ahmad <aalahmary0032@stu.kau.edu.sa>
2022-10-04 23:09:25 +0300

    Experimenting with MACD and EMA on 1m timeframe

--- user_data/strategies/ShortTerm.py
+++ user_data/strategies/ShortTerm.py
@@ -22,19 +22,22 @@ class ShortTerm(IStrategy):
     # Minimal ROI designed for the strategy.
     # adjust based on market conditions. We would recommend to keep it low for quick turn arounds
     # This attribute will be overridden if the config file contains "minimal_roi"
+
     minimal_roi = {
-        "120": 0.03,
-        "60": 0.05,
-        "35": 0.067,
+        "250": 0.01,
+        "100": 0.03,
+        "80": 0.05,
+        "40": 0.067,
         "0": 0.09
     }
 
-    use_custom_stoploss = True
+    # macd_up = DecimalParameter(high=15, low=4, default=8, space='buy')
+    # macd_down = DecimalParameter(high=-4, low=-15, default=-8, space='buy')
 
     # Trailing stoploss
-    trailing_stop = False
-    trailing_stop_positive = 0.012
-    trailing_stop_positive_offset = 0.092
+    trailing_stop = True
+    trailing_stop_positive = 0.01
+    trailing_stop_positive_offset = 0.03
     trailing_only_offset_is_reached = False
 
     # Can this strategy go short?
@@ -44,7 +47,7 @@ class ShortTerm(IStrategy):
     stoploss = -0.03
 
     # Optimal timeframe for the strategy
-    timeframe = '5m'
+    timeframe = '1m'
 
     # Strategy Parameters
     # buy_stoch = DecimalParameter(1, 33, default=BUY_STOCH_DEF)
@@ -59,39 +62,26 @@ class ShortTerm(IStrategy):
     ignore_roi_if_entry_signal = False
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        EMA = ta.trend.EMAIndicator(dataframe['close'], window=9)
-        BB = ta.volatility.BollingerBands(dataframe['close'], window=16)
-        MACD = ta.trend.MACD(dataframe['close'], window_slow=22, window_fast=8)
-        dataframe['ema'] = EMA.ema_indicator()
-        dataframe['bb_h'] = BB.bollinger_hband()
-        dataframe['bb_m'] = BB.bollinger_mavg()
-        dataframe['bb_l'] = BB.bollinger_lband()
-        dataframe['bb_w'] = BB.bollinger_wband()
-        dataframe['macd'] = MACD.macd()
-        dataframe['macdsignal'] = MACD.macd_signal()
+        MACD = ta.trend.MACD(dataframe['close'])
+        EMA7 = ta.trend.EMAIndicator(dataframe['close'], window=7)
+        EMA14 = ta.trend.EMAIndicator(dataframe['close'], window=14)
+        dataframe['macdhist'] = MACD.macd_diff()
+        dataframe['ema7'] = EMA7.ema_indicator()
+        dataframe['ema14'] = EMA14.ema_indicator()
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                (qtpylib.crossed_above(dataframe['ema'], dataframe['bb_m']) |
-                 dataframe['close'] <= dataframe['bb_l']) &
-                ((dataframe['macd'] > 8) | (dataframe['macd'] < -8)) &
-                (dataframe['bb_w'] > 0.15)
-            ),
-            'enter_long'] = 1
-        # dataframe.loc[
-        #     (
-        #
-        #     ),
-        # ] = 1
+                (qtpylib.crossed_below(dataframe['macdhist'], 0)) &
+                (dataframe['ema7'] > dataframe['ema14'])
+            ), 'enter_long'] = 1
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                (qtpylib.crossed_below(dataframe['ema'], dataframe['bb_m']) |
-                dataframe['close'] >= dataframe['bb_h'])
+
             ),
             'exit_long'] = 1
         return dataframe
